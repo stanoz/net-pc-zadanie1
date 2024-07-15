56bdb7a6d28a2c1039bc1d026256dd0e45fbfec1
stanoz <s.oziemczuk@gmail.com>
2024-07-15 21:19:04 +0200

    refactor

--- zadanie1Backend/zadanie1Backend/Dtos/Contact/GetContactDto.cs
+++ zadanie1Backend/zadanie1Backend/Dtos/Contact/GetContactDto.cs
@@ -3,6 +3,7 @@
 public class GetContactDto
 {
     public string Name { get; set; }
+    public string Surname { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public GetCategoryDto Category { get; set; }
--- zadanie1Backend/zadanie1Backend/Dtos/Contact/PostAndPutContactDto.cs
+++ zadanie1Backend/zadanie1Backend/Dtos/Contact/PostAndPutContactDto.cs
@@ -3,6 +3,7 @@
 public class PostAndPutContactDto
 {
     public string Name { get; set; }
+    public string Surname { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public PostAndPutCategoryDto Category { get; set; }
