bdb37c7d32f9c327f92c7154b0aab1a9feb0477a
stanoz <s.oziemczuk@gmail.com>
2024-07-15 17:02:01 +0200

    add DataContext

---
+++ zadanie1Backend/zadanie1Backend/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using zadanie1Backend.Models;
+
+namespace zadanie1Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Contact> Contacts => Set<Contact>();
+    public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contact>()
+            .HasIndex(c => c.Email)
+            .IsUnique();
+    }
+}
--- zadanie1Backend/zadanie1Backend/Program.cs
+++ zadanie1Backend/zadanie1Backend/Program.cs
@@ -1,15 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using zadanie1Backend.Data;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
