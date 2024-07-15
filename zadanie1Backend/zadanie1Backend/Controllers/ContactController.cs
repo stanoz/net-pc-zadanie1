5b34ed27956a7d70e3f66547a986a1fb9dbad575
stanoz <s.oziemczuk@gmail.com>
2024-07-15 17:35:40 +0200

    add controller class base

---
+++ zadanie1Backend/zadanie1Backend/Controllers/ContactController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace zadanie1Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContactController : ControllerBase
+{
+    
+}
